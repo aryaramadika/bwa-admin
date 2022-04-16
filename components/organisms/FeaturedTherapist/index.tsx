dc0eb2f531b66066a9333ad98dee4614ffbcdc52
Rama <aryaramadika78@gmail.com>
2022-04-16 23:20:09 +0800

    dynamic therapist and pricedetail

--- components/organisms/FeaturedTherapist/index.tsx
+++ components/organisms/FeaturedTherapist/index.tsx
@@ -2,6 +2,8 @@ import TherapistStaff from "../../molecules/TherapistStaff"
 import { useCallback,useEffect, useState } from "react"
 import axios from "axios"
 import { getFeaturedTherapist } from "../../../services/patient"
+import { TherapistStaffTypes } from "../../../services/data-types"
+
 export default function FeaturedTherapist() {
     const [therapistList, setTherapistList] = useState([])
     const getFeaturedTherapistList = useCallback(async ()=>{
@@ -9,12 +11,16 @@ export default function FeaturedTherapist() {
         setTherapistList(data)
         console.log(data)
     }, [getFeaturedTherapist])
-    useEffect(async () => {
-    //    const response = await axios.get('http://localhost:2000/api/v1/patients/landingpage');
+    
+    useEffect( () => {
+    //    const response = await axios.get('https://admintbs5.herokuapp.com/api/v1/patients/landingpage');
     //     console.log('data :', response.data.data);
     //     setTherapistList(response.data.data);
+    //     console.log(therapistList)
+
     getFeaturedTherapistList()
     },[])
+    const API_IMG = process.env.NEXT_PUBLIC_IMAGE
     return (
         <section className="featured-game pt-50 pb-50">
     <div className="container-fluid" id="fisioterapi">
@@ -23,14 +29,17 @@ export default function FeaturedTherapist() {
         <div className="d-flex flex-row flex-lg-wrap overflow-setting justify-content-lg-between gap-lg-3 gap-4"
             data-aos="fade-up">
 
-            {therapistList.map(staff =>{
-                <TherapistStaff 
+            {therapistList.map((staff: TherapistStaffTypes) =>{
+                return (<TherapistStaff 
                 key={staff._id} 
                 nameStaff={staff.therapistName} 
                 ageStaff = {staff.therapistAge} 
-                thumbnail={`http://localhost:2000/api/v1/uploads/${staff.thumbnail}`}/>
-
+                thumbnail={`${API_IMG}/${staff.thumbnail}`}
+                id={staff._id}
+                />)
+                
             })}    
+    
             
             
         </div>
